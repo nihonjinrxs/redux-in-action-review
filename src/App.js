7a1af8b6317f650d97e67c052fe6af7c1b281291
Ryan B. Harvey <ryan.b.harvey@ted.com>
2017-06-17 02:39:13 -0500

    Sec 2.6 as in book. Broken until you 'yarn add react-redux', but no instruction to do so in section.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import TasksPage from './components/TasksPage';
 
 class App extends Component {
   render() {
     return (
       <div className="main-content">
-        <TasksPage tasks={mockTasks} />
+        <TasksPage tasks={this.props.tasks} />
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) { //#A
+  return {
+    tasks: state.tasks
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
+import { Provider } from 'redux';
 import tasks from './reducers'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const store = createStore(tasks)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}> //#B
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
